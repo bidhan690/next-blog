bed99fdc2a01eaefac4a5693adc44a995e56a783
Bidhan Niroula <116049938+bidhan690@users.noreply.github.com>
2023-02-26 13:30:17 +0000

    all done

--- posts/mastering-javascript.md
+++ posts/mastering-javascript.md
@@ -10,12 +10,12 @@ JavaScript powers the web - it's **the** most important programming language you
 
 For example, you should understand code like this:
 
-```js
+~~~js
 const basics = 'Okay, that should not be too difficult actually';
 
 function printBasics() { console.log(basics): }
 
 printBasics();
-```
+~~~
 
-Learn more about it [here](https://academind.com).
+Learn more about it [here](https://google.com).
--- src/components/posts/post-detail/post-content.jsx
+++ src/components/posts/post-detail/post-content.jsx
@@ -1,17 +1,20 @@
-import ReactMarkdown from 'react-markdown';
-import styles from './post-content.module.css';
-import PostHeader from './post-header';
+import ReactMarkdown from "react-markdown";
+import styles from "./post-content.module.css";
+import PostHeader from "./post-header";
+import Image from "next/image";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 
-import Image from 'next/image';
-
+SyntaxHighlighter.registerLanguage("js", js);
 
 export default function PostContent(props) {
   const post = props.posts;
   const ob = {
     paragraph(paragraph) {
-      const {node} = paragraph;
+      const { node } = paragraph;
 
-      if (node.children[0].type === 'img') {
+      if (node.children[0].type === "img") {
         const img = node.children[0];
         return (
           <div className={styles.image}>
@@ -21,15 +24,28 @@ export default function PostContent(props) {
       }
       return <p>{paragraph.children}</p>;
     },
-    // code(code) {
-    //   const {language, value} = code;
-    //   return <SyntaxHighlighter language={language} style={a11yDark} children={value} />;
-    // },
+
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={match[1]}
+          PreTag="div"
+          {...props}
+        >
+          {String(children).replace(/\n$/, "")}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
   };
 
   return (
     <>
-     
       <article className={styles.content}>
         <PostHeader title={post.title} image={post.image} />
         <ReactMarkdown components={ob}>{post.content}</ReactMarkdown>
